585d89c718ad1157b29893aec0b97aa5f1cee5cf
danjquinn42 <danjquinn42@gmail.com>
2017-05-16 18:18:59 -0400

    Ignore non-implemented tests

--- javatest/src/test/java/createpage/GuestUserCreatesTheme.java
+++ javatest/src/test/java/createpage/GuestUserCreatesTheme.java
@@ -1,12 +1,12 @@
 package createpage;
 
+import org.junit.Ignore;
 import org.junit.Test;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class GuestUserCreatesTheme {
+    @Ignore("Not implemented")
     @Test
     public void guestUserThemeSearchable() {
-        throw new NotImplementedException();
-//todo
+        //todo
     }
 }
