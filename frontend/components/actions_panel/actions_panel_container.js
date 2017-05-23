b9dc233ff3c4673db6f48c06dfcb5d98c7340adf
danjquinn42 <danjquinn42@gmail.com>
2017-05-23 15:50:27 -0400

    Fill actions panel boilerplate

---
+++ frontend/components/actions_panel/actions_panel_container.js
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import { updateTheme } from '../../actions/theme_actions';
+
+import ActionsPanel from './actions_panel';
+
+const mapStateToProps = ( { session } ) => {
+  return {
+    user: session.currentUser
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    updateTheme: (theme) => dispatch(updateTheme(theme))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ThemeView);
